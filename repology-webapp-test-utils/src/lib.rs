6b784be01ded8819ff71996082a61a56e9a2fca6
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-09-11 02:42:00 +0300

    Move testing code into separate crate

---
+++ repology-webapp-test-utils/src/lib.rs
@@ -0,0 +1,146 @@
+// SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pub mod __private {
+    pub use axum;
+    pub use json;
+    pub use mime;
+    pub use pretty_assertions;
+    pub use sxd_document;
+    pub use sxd_xpath;
+
+    use tower_service::Service;
+
+    use repology_webapp::create_app;
+
+    pub struct Response {
+        pub status: axum::http::StatusCode,
+        pub content_type: Option<String>,
+        pub body: String,
+    }
+
+    pub async fn get(pool: sqlx::PgPool, uri: &str) -> Result<Response, anyhow::Error> {
+        let mut app = create_app(pool).await?;
+        let response = app
+            .call(
+                axum::http::Request::builder()
+                    .uri(uri)
+                    .method("GET")
+                    .body("".to_owned())?,
+            )
+            .await?;
+        Ok(Response {
+            status: response.status(),
+            content_type: response
+                .headers()
+                .get(axum::http::header::CONTENT_TYPE)
+                .and_then(|value| value.to_str().ok().map(|value| value.into())),
+            body: std::str::from_utf8(&axum::body::to_bytes(response.into_body(), 10000).await?)?
+                .into(),
+        })
+    }
+
+    pub trait EqualsToXpathValue {
+        fn equals_to_xpath_value(&self, v: &sxd_xpath::Value) -> bool;
+    }
+
+    impl EqualsToXpathValue for bool {
+        fn equals_to_xpath_value(&self, v: &sxd_xpath::Value) -> bool {
+            match v {
+                sxd_xpath::Value::Boolean(b) => b == self,
+                _ => false,
+            }
+        }
+    }
+
+    impl EqualsToXpathValue for &str {
+        fn equals_to_xpath_value(&self, v: &sxd_xpath::Value) -> bool {
+            match v {
+                sxd_xpath::Value::String(s) => s == self,
+                _ => false,
+            }
+        }
+    }
+
+    impl EqualsToXpathValue for f64 {
+        fn equals_to_xpath_value(&self, v: &sxd_xpath::Value) -> bool {
+            match v {
+                sxd_xpath::Value::Number(f) => f == self,
+                _ => false,
+            }
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! check_code {
+    ($pool:ident, $uri:literal, $code:ident) => {
+        let resp = $crate::__private::get($pool.clone(), $uri).await.unwrap();
+        assert_eq!(
+            resp.status,
+            $crate::__private::axum::http::StatusCode::$code
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! check_json {
+    ($pool:ident, $uri:literal, $expected_json:literal) => {
+        let resp = $crate::__private::get($pool.clone(), $uri).await.unwrap();
+        assert_eq!(resp.status, $crate::__private::axum::http::StatusCode::OK);
+        assert_eq!(
+            resp.content_type,
+            Some($crate::__private::mime::APPLICATION_JSON.as_ref().into())
+        );
+
+        let returned = $crate::__private::json::stringify_pretty(
+            $crate::__private::json::parse(&resp.body).unwrap(),
+            4,
+        );
+        let expected = $crate::__private::json::stringify_pretty(
+            $crate::__private::json::parse($expected_json).unwrap(),
+            4,
+        );
+        $crate::__private::pretty_assertions::assert_eq!(returned, expected);
+    };
+}
+
+#[macro_export]
+macro_rules! check_svg {
+    ($pool:ident, $uri:literal $(, $($has:literal)? $(!$hasnt:literal)? $(@$xpath_expr:literal==$xpath_value:literal)?)*) => {
+        let resp = $crate::__private::get($pool.clone(), $uri)
+            .await
+            .unwrap();
+        assert_eq!(resp.status, $crate::__private::axum::http::StatusCode::OK);
+        assert_eq!(resp.content_type, Some($crate::__private::mime::IMAGE_SVG.as_ref().into()));
+
+        let parsed = $crate::__private::sxd_document::parser::parse(&resp.body);
+        assert!(parsed.is_ok(), "failed to parse XML document");
+        let parsed = parsed.unwrap();
+        let _document = parsed.as_document();
+
+        $(
+            $(
+                assert!(resp.body.contains($has));
+            )?
+            $(
+                assert!(!resp.body.contains($hasnt));
+            )?
+            $(
+                {
+                    use $crate::__private::EqualsToXpathValue;
+
+                    let factory = $crate::__private::sxd_xpath::Factory::new();
+                    let xpath = factory.build($xpath_expr).unwrap();
+                    let xpath = xpath.unwrap();
+
+                    let mut context = $crate::__private::sxd_xpath::Context::new();
+                    context.set_namespace("svg", "http://www.w3.org/2000/svg");
+
+                    let xpath_res = xpath.evaluate(&context, _document.root()).unwrap();
+                    assert!($xpath_value.equals_to_xpath_value(&xpath_res), "unexpected xpath value {:?} while expected \"{}\"", xpath_res, $xpath_value);
+                }
+            )?
+        )*
+    };
+}
--- repology-webapp/tests/api.rs
+++ repology-webapp/tests/api.rs
@@ -4,61 +4,9 @@
 #![feature(coverage_attribute)]
 #![coverage(off)]
 
-use anyhow::Error;
-use axum::http::{header, Request, StatusCode};
 use sqlx::PgPool;
-use tower_service::Service;
 
-use repology_webapp::create_app;
-
-struct Response {
-    pub status: StatusCode,
-    pub content_type: Option<String>,
-    pub body: String,
-}
-
-async fn get(pool: PgPool, uri: &str) -> Result<Response, Error> {
-    let mut app = create_app(pool).await?;
-    let response = app
-        .call(
-            Request::builder()
-                .uri(uri)
-                .method("GET")
-                .body("".to_owned())?,
-        )
-        .await?;
-    Ok(Response {
-        status: response.status(),
-        content_type: response
-            .headers()
-            .get(header::CONTENT_TYPE)
-            .and_then(|value| value.to_str().ok().map(|value| value.into())),
-        body: std::str::from_utf8(&axum::body::to_bytes(response.into_body(), 10000).await?)?
-            .into(),
-    })
-}
-
-macro_rules! check_code {
-    ($pool:ident, $uri:literal, $code:ident) => {
-        let resp = get($pool.clone(), $uri).await.unwrap();
-        assert_eq!(resp.status, StatusCode::$code);
-    };
-}
-
-macro_rules! check_json {
-    ($pool:ident, $uri:literal, $expected_json:literal) => {
-        let resp = get($pool.clone(), $uri).await.unwrap();
-        assert_eq!(resp.status, StatusCode::OK);
-        assert_eq!(
-            resp.content_type,
-            Some(mime::APPLICATION_JSON.as_ref().into())
-        );
-
-        let returned = json::stringify_pretty(json::parse(&resp.body).unwrap(), 2);
-        let expected = json::stringify_pretty(json::parse($expected_json).unwrap(), 2);
-        pretty_assertions::assert_eq!(returned, expected);
-    };
-}
+use repology_webapp_test_utils::check_json;
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("api_data"))]
 async fn test_badge_tiny_repos(pool: PgPool) {
