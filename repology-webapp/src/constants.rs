b31df2ec567af4ec50d0adefdedcc3247a01d04f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-10-15 02:23:26 +0300

    webapp: Import all PER_PAGE constants

--- repology-webapp/src/constants.rs
+++ repology-webapp/src/constants.rs
@@ -4,4 +4,20 @@
 // TODO: make this configurable
 pub const REPOLOGY_HOSTNAME: &str = "https://repology.org";
 
+#[expect(unused)]
+pub const CVES_PER_PAGE: u32 = 200;
+#[expect(unused)]
+pub const HISTORY_PER_PAGE: u32 = 500;
+#[expect(unused)]
+pub const MAINTAINERS_PER_PAGE: u32 = 200;
+#[expect(unused)]
+pub const PROBLEMS_PER_PAGE: u32 = 200;
 pub const PROJECTS_PER_PAGE: u32 = 200;
+#[expect(unused)]
+pub const REDIRECTS_PER_PAGE: u32 = 200;
+#[expect(unused)]
+pub const REPORTS_PER_PAGE: u32 = 100;
+#[expect(unused)]
+pub const TRENDING_PER_PAGE: u32 = 25;
+#[expect(unused)]
+pub const TURNOVER_PER_PAGE: u32 = 350;
