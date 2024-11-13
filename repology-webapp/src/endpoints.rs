35d4e89b07b331387da42a02deea467912bf6020
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-11-13 03:48:08 +0300

    webapp: Remove dead code

--- repology-webapp/src/endpoints.rs
+++ repology-webapp/src/endpoints.rs
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::collections::HashMap;
-
-use anyhow::{anyhow, bail, Error};
-use serde_json::Value;
 use strum::EnumProperty;
 use strum_macros::{EnumString, IntoStaticStr};
 
@@ -321,43 +317,6 @@ impl Endpoint {
         self.get_str("section")
             .is_some_and(|endpoint_section| Section::from_str(endpoint_section).unwrap() == section)
     }
-
-    pub fn construct_url(&self, values: &HashMap<String, Value>) -> Result<String, Error> {
-        let is_key_char = |c: char| c.is_lowercase() || c == '_';
-
-        let mut rest = self.path();
-        let mut res = String::new();
-
-        loop {
-            if let Some((prefix, key_and_rest)) = rest.split_once(":") {
-                res += prefix;
-
-                let (key, suffix) = key_and_rest.split_at(
-                    key_and_rest
-                        .find(|c| !is_key_char(c))
-                        .unwrap_or(key_and_rest.len()),
-                );
-
-                match values
-                    .get(key)
-                    .ok_or(anyhow!("missing value for path placeholder \"{}\"", key))?
-                {
-                    Value::Number(n) => {
-                        res += &n.to_string();
-                    }
-                    Value::String(s) => {
-                        res += s;
-                    }
-                    _ => {
-                        bail!("invalid value type for path placeholder \"{}\"", key);
-                    }
-                };
-                rest = suffix;
-            } else {
-                return Ok(res + rest);
-            }
-        }
-    }
 }
 
 #[cfg(test)]
@@ -376,36 +335,5 @@ mod tests {
     #[test]
     fn test_name() {
         assert_eq!(Endpoint::BadgeVersionForRepo.name(), "BadgeVersionForRepo");
-    }
-
-    #[test]
-    fn test_construct_url() {
-        use serde_json::json;
-        let repository_name: HashMap<String, Value> = [("repository_name".into(), json!("foo"))]
-            .into_iter()
-            .collect();
-        let project_name: HashMap<String, Value> = [("project_name".into(), json!("bar"))]
-            .into_iter()
-            .collect();
-        let project_and_repository_names = {
-            let mut t = project_name.clone();
-            t.extend(repository_name.clone().into_iter());
-            t
-        };
-
-        assert_eq!(
-            Endpoint::ApiV1Project.construct_url(&project_name).unwrap(),
-            "/api/v1/project/bar"
-        );
-
-        assert_eq!(
-            Endpoint::BadgeVersionForRepo
-                .construct_url(&project_and_repository_names)
-                .unwrap(),
-            "/badge/version-for-repo/foo/bar.svg"
-        );
-        assert!(Endpoint::BadgeVersionForRepo
-            .construct_url(&repository_name)
-            .is_err());
     }
 }
