45f7a4b16be30c68a8795c529da3152ee5a927fb
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-12-16 23:28:42 +0300

    webapp: Make trivial pages tests granular

--- repology-webapp/tests/integration_tests/trivial_pages.rs
+++ repology-webapp/tests/integration_tests/trivial_pages.rs
@@ -6,14 +6,46 @@ use sqlx::PgPool;
 use repology_webapp_test_utils::check_response;
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR")]
-async fn test_trivial_pages(pool: PgPool) {
+async fn test_api(pool: PgPool) {
     check_response!(pool, "/api", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Terms of use");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_api_v1(pool: PgPool) {
     check_response!(pool, "/api/v1", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Terms of use");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_about(pool: PgPool) {
     check_response!(pool, "/docs/about", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "About");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_bots(pool: PgPool) {
     check_response!(pool, "/docs/bots", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "+https://repology.org/docs/bots");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs(pool: PgPool) {
     check_response!(pool, "/docs", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Documentation");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_not_supported(pool: PgPool) {
     check_response!(pool, "/docs/not_supported", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Hyperbola");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_requirements(pool: PgPool) {
     check_response!(pool, "/docs/requirements", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Rational");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_news(pool: PgPool) {
     check_response!(pool, "/news", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Added");
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_tools(pool: PgPool) {
     check_response!(pool, "/tools", status OK, content_type "text/html", html_ok "allow_empty_tags,warnings_fatal", contains "Project by package name");
 }
--- repology-webapp/tests/snapshot_tests/trivial_pages.rs
+++ repology-webapp/tests/snapshot_tests/trivial_pages.rs
@@ -6,14 +6,46 @@ use sqlx::PgPool;
 use super::uri_snapshot_test;
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR")]
-async fn test_trivial_pages(pool: PgPool) {
+async fn test_api(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/api").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_api_v1(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/api/v1").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_about(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/docs/about").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_bots(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/docs/bots").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/docs").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_not_supported(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/docs/not_supported").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_docs_requirements(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/docs/requirements").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_news(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/news").await;
+}
+
+#[sqlx::test(migrator = "repology_common::MIGRATOR")]
+async fn test_tools(pool: PgPool) {
     uri_snapshot_test(pool.clone(), "/tools").await;
 }
