903ed25f960e46331415fc04e6cc876e5251c075
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-02-08 04:47:40 +0300

    webapp: Convert opensearch integration tests to new framework

--- repology-webapp/tests/integration_tests/opensearch.rs
+++ repology-webapp/tests/integration_tests/opensearch.rs
@@ -3,17 +3,26 @@
 
 use sqlx::PgPool;
 
+use repology_webapp_test_utils::Request;
 use repology_webapp_test_utils::check_response;
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR")]
 async fn test_maintainer(pool: PgPool) {
-    check_response!(
-        pool,
-        "/opensearch/maintainer.xml",
-        status OK,
-        content_type "application/xml",
-        contains "={searchTerms}",
-        xpath "string(/*[local-name()='OpenSearchDescription']/*[local-name()='ShortName'])" "Repology maintainers"
+    let response = Request::new(pool, "/opensearch/maintainer.xml")
+        .with_xml_namespace("os", "http://a9.com/-/spec/opensearch/1.1/")
+        .perform()
+        .await;
+    assert_eq!(response.status(), http::StatusCode::OK);
+    assert_eq!(
+        response.header_value_str("content-type").unwrap(),
+        Some("application/xml")
+    );
+    assert!(response.text().unwrap().contains("={searchTerms}"));
+    assert_eq!(
+        response
+            .xpath("string(/os:OpenSearchDescription/os:ShortName)")
+            .unwrap(),
+        "Repology maintainers"
     );
 }
 
