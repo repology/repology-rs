b4682c45daf5858c76195b5aad257dfccc8746ab
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-02-10 12:49:31 +0300

    webapp: Convert another opensearch test for new framework

--- repology-webapp/tests/integration_tests/opensearch.rs
+++ repology-webapp/tests/integration_tests/opensearch.rs
@@ -4,7 +4,6 @@
 use sqlx::PgPool;
 
 use repology_webapp_test_utils::Request;
-use repology_webapp_test_utils::check_response;
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR")]
 async fn test_maintainer(pool: PgPool) {
@@ -17,12 +16,9 @@ async fn test_maintainer(pool: PgPool) {
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR")]
 async fn test_project(pool: PgPool) {
-    check_response!(
-        pool,
-        "/opensearch/project.xml",
-        status OK,
-        content_type "application/xml",
-        contains "={searchTerms}",
-        xpath "string(/*[local-name()='OpenSearchDescription']/*[local-name()='ShortName'])" "Repology projects"
-    );
+    let response = Request::new(pool, "/opensearch/project.xml").with_xml_namespace("os", "http://a9.com/-/spec/opensearch/1.1/").perform().await;
+    assert_eq!(response.status(), http::StatusCode::OK);
+    assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/xml"));
+    assert!(response.text().unwrap().contains("={searchTerms}"));
+    assert_eq!(response.xpath("string(/os:OpenSearchDescription/os:ShortName)").unwrap(), "Repology projects");
 }
