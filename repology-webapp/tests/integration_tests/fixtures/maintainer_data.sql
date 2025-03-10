876f816e6f4986b72945b7ddf9f2f281e2e7b9c7
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-03-10 23:10:48 +0300

    webapp: Tweak maintainers fixture for more stable date interval

--- repology-webapp/tests/integration_tests/fixtures/maintainer_data.sql
+++ repology-webapp/tests/integration_tests/fixtures/maintainer_data.sql
@@ -2,7 +2,7 @@ INSERT INTO maintainers(id, maintainer, orphaned_at) VALUES
 	(1, 'orphaned@example.com', '2024-01-01 00:00:00'),
 	(2, 'orphaned-in-future@example.com', '2124-01-01 00:00:00');
 
-INSERT INTO maintainers(id, maintainer, num_packages, num_projects, counts_per_repo, num_projects_per_category) VALUES
-	(3, 'active@example.com', 10, 10, '{"freebsd":[10,11,12,13,14,15]}'::jsonb, '{"games":10}'::jsonb),
-	(4, 'fallback-mnt-foo@repology', 1, 1, '{"freebsd":[1,1,1,1,1,1]}'::jsonb, '{"games":10}'::jsonb),
-	(5, 'no-vuln-column@example.com', 10, 10, '{"freebsd":[10,11,12,13,14]}'::jsonb, '{"games":10}'::jsonb);
+INSERT INTO maintainers(id, maintainer, num_packages, num_projects, counts_per_repo, num_projects_per_category, first_seen) VALUES
+	(3, 'active@example.com', 10, 10, '{"freebsd":[10,11,12,13,14,15]}'::jsonb, '{"games":10}'::jsonb, now() - interval '370 day'),
+	(4, 'fallback-mnt-foo@repology', 1, 1, '{"freebsd":[1,1,1,1,1,1]}'::jsonb, '{"games":10}'::jsonb, now() - interval '370 day'),
+	(5, 'no-vuln-column@example.com', 10, 10, '{"freebsd":[10,11,12,13,14]}'::jsonb, '{"games":10}'::jsonb, now() - interval '370 day');
