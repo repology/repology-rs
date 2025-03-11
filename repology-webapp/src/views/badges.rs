783cd3421b281c1515273c3b68c9363050264710
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-03-11 04:19:13 +0300

    webapp: Remove more unneeded pubs

--- repology-webapp/src/views/badges.rs
+++ repology-webapp/src/views/badges.rs
@@ -1,12 +1,12 @@
 // SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pub mod latest_versions;
-pub mod repository_big;
-pub mod tiny_repos;
-pub mod version_for_repo;
-pub mod versions_matrix;
-pub mod vertical_allrepos;
+mod latest_versions;
+mod repository_big;
+mod tiny_repos;
+mod version_for_repo;
+mod versions_matrix;
+mod vertical_allrepos;
 
 pub use latest_versions::*;
 pub use repository_big::*;
