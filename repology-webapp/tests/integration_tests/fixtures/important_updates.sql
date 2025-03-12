906e2eaefe2bde96161339cbbd623082b705af00
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-03-13 02:57:59 +0300

    webapp: Tweak time offset for more stable tests
    `now() - 23 hours` could be interpreted as `22 hours ago` sometimes
    (not sure why, as fixture should be applied strictly before request is
    performed), but 23.5 hours would be `23 hoours ago` within 30 minute
    jitter, which should be more that enough for stable tests.

--- repology-webapp/tests/integration_tests/fixtures/important_updates.sql
+++ repology-webapp/tests/integration_tests/fixtures/important_updates.sql
@@ -1,2 +1,2 @@
 INSERT INTO global_version_events(effname, ts, type, spread, data) VALUES
-	('zsh', now() - interval '23 hours', 'newest_update', 6, '{"versions": ["1.2.3"]}'::jsonb);
+	('zsh', now() - interval '23.5 hours', 'newest_update', 6, '{"versions": ["1.2.3"]}'::jsonb);
