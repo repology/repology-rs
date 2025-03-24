f4f3608391e23e216e31587900b6e241602f6265
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-03-25 00:05:44 +0300

    vulnupdater: Revert "Ignore NVD server time for now (for it is broken)"
    This reverts commit 5fb206b43c544ad2c222d45e4a803da232191e51.
    
    NVD was fixed.

--- repology-vulnupdater/src/fetcher.rs
+++ repology-vulnupdater/src/fetcher.rs
@@ -124,11 +124,7 @@ impl<'a> Paginator<'a> {
         let client_time = Utc::now();
         let time_offset = (server_time - client_time).abs();
         if time_offset > MAX_TIME_OFFSET {
-            // XXX: ATOW (2025-03-12) cves API timestamp is offset by 4 hours to the past,
-            // while cpes API timestamp is correct UTC time. So for now ignore time
-            // difference
-            //bail!(
-            tracing::error!(
+            bail!(
                 "too big time offset ({}) between client ({:?}) and server ({:?})",
                 time_offset,
                 client_time,
