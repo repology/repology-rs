f82657f5160928d690d7204793ed835df4940c4f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-04-04 15:56:06 +0300

    admin: Rename button so it fits on mobile

--- repology-admin/src/components/report.rs
+++ repology-admin/src/components/report.rs
@@ -150,7 +150,7 @@ impl Report {
                     .card-footer {
                         button .card-footer-item .has-text-success type="submit" name="accepted" value="true" { "Accept" }
                         button .card-footer-item .has-text-danger type="submit" name="accepted" value="false"{ "Reject" }
-                        button .card-footer-item type="submit" name="accepted" value="undefined" { "Leave unprocessed" }
+                        button .card-footer-item type="submit" name="accepted" value="undefined" { "Pending" }
                         button .card-footer-item hx-delete={"/parts/report/" (self.id)} hx-confirm="Really delete this report?" hx-swap="delete" { "Delete" }
                     }
                 }
