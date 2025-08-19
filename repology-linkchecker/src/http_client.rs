1aa01c4eb0ef3592a8f5198aad3eeaa2a984890f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-08-19 17:18:00 +0300

    linkchecker: disable HTTP retries
    reqwest 0.1.23 now allows to customize retry policy. The default has
    not changed and is to only retry "low level protocol NACKs", which,
    as explained in [1], implies recreating presistent HTTP/2 connections
    transparently when needed. That mostly does not affect us as we create
    a new Client for each request, and thus do not have any persistent
    connections, but still
    - be explicit that we don't expect any retries.
    - handle the rare cases that the request gets such low level protocol
      NACK right away - report an error, do not retry
    
    [1] https://github.com/seanmonstar/reqwest/pull/2763#issuecomment-3200851949

--- repology-linkchecker/src/http_client.rs
+++ repology-linkchecker/src/http_client.rs
@@ -75,6 +75,7 @@ impl HttpClient {
             .redirect(reqwest::redirect::Policy::none())
             .dns_resolver(Arc::new(FakeResolver::new(request.address)))
             .use_rustls_tls()
+            .retry(reqwest::retry::never())
             .build()
             .expect("expected to always be able to build reqwest client");
 
