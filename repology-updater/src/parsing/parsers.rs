94982c66e1e591903bd4e9dd0ee206d2e5535ed0
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-10-18 01:09:57 +0300

    updater: Allow to spawn parser by name

--- repology-updater/src/parsing/parsers.rs
+++ repology-updater/src/parsing/parsers.rs
@@ -10,3 +10,15 @@ pub use freebsd::*;
 pub use stalix::*;
 pub use tincan::*;
 pub use yacp::*;
+
+use crate::parsing::parser::PackageParser;
+
+pub fn create_parser(name: &str) -> anyhow::Result<Box<dyn PackageParser>> {
+    match name {
+        "FreeBsdParser" => Ok(Box::new(freebsd::FreeBsdParser {})),
+        "StalIxParser" => Ok(Box::new(stalix::StalIxParser {})),
+        "TinCanParser" => Ok(Box::new(tincan::TinCanParser {})),
+        "YacpParser" => Ok(Box::new(yacp::YacpParser {})),
+        _ => Err(anyhow::anyhow!("invalid parser name {}", name)),
+    }
+}
