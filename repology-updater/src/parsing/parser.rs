9707d46d96cb2722a22b2c4d3222d0ed914bee4a
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-10-18 01:10:35 +0300

    updater: Implement more package sinks

--- repology-updater/src/parsing/parser.rs
+++ repology-updater/src/parsing/parser.rs
@@ -26,3 +26,46 @@ impl PackageSink for PackageAccumulator {
         Ok(())
     }
 }
+
+#[derive(Default)]
+pub struct PackageNullSink {}
+
+impl PackageSink for PackageNullSink {
+    fn push(&mut self, _: PackageMaker) -> Result<(), PackageParsingError> {
+        Ok(())
+    }
+}
+
+pub struct PackageCounter<'n, N: ?Sized> {
+    pub count: usize,
+    next: &'n mut N,
+}
+
+impl<'n, N> PackageCounter<'n, N>
+where
+    N: PackageSink + ?Sized,
+{
+    pub fn new(next: &'n mut N) -> Self {
+        Self { count: 0, next }
+    }
+}
+
+impl<'n, N> PackageSink for PackageCounter<'n, N>
+where
+    N: PackageSink + ?Sized,
+{
+    fn push(&mut self, package_maker: PackageMaker) -> Result<(), PackageParsingError> {
+        self.count += 1;
+        self.next.push(package_maker)
+    }
+}
+
+#[derive(Default)]
+pub struct PackageDumper {}
+
+impl PackageSink for PackageDumper {
+    fn push(&mut self, package_maker: PackageMaker) -> Result<(), PackageParsingError> {
+        println!("{:#?}", package_maker.finalize()?);
+        Ok(())
+    }
+}
