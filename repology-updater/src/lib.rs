f089a2b2eb9e2a85864fc55482ba848823134f94
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-11-02 01:55:27 +0300

    updater: Sort features

--- repology-updater/src/lib.rs
+++ repology-updater/src/lib.rs
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: Copyright 2025 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#![feature(file_buffered)]
-#![feature(coverage_attribute)]
-#![feature(iter_collect_into)]
 #![feature(const_trait_impl)]
+#![feature(coverage_attribute)]
+#![feature(debug_closure_helpers)]
+#![feature(file_buffered)]
+#![feature(iter_collect_into)]
 #![feature(test)]
 #![feature(trait_alias)]
-#![feature(debug_closure_helpers)]
 
 pub mod fetching;
 pub mod package;
