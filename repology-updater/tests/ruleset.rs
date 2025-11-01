a90c7932f7681aac97db808d73b5d5bf428da277
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-11-01 19:14:38 +0300

    updater: Shorten ruleset test names

--- repology-updater/tests/ruleset.rs
+++ repology-updater/tests/ruleset.rs
@@ -6,28 +6,28 @@ use std::path::Path;
 use repology_updater::ruleset::Ruleset;
 
 #[test]
-fn test_parse_ruleset_hier() {
+fn test_hier() {
     let path = Path::new("tests/ruleset_test_data/hier");
     let res = Ruleset::parse(path).expect("ruleset should be parsable");
     insta::assert_debug_snapshot!(res);
 }
 
 #[test]
-fn test_parse_ruleset_simple_cases() {
+fn test_simple_cases() {
     let path = Path::new("tests/ruleset_test_data/simple_cases");
     let res = Ruleset::parse(path).expect("ruleset should be parsable");
     insta::assert_debug_snapshot!(res);
 }
 
 #[test]
-fn test_parse_ruleset_allflags() {
+fn test_allflags() {
     let path = Path::new("tests/ruleset_test_data/allflags");
     let res = Ruleset::parse(path).expect("ruleset should be parsable");
     insta::assert_debug_snapshot!(res);
 }
 
 #[test]
-fn test_parse_ruleset_error_duplicate_key() {
+fn test_error_duplicate_key() {
     let path = Path::new("tests/ruleset_test_data/error_duplicate_key");
     assert!(Ruleset::parse(path).is_err());
 }
