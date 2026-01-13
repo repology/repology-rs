7c4a1a9c2e741f0a885547745678f83797ad94ed
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-01-13 16:12:24 +0300

    webapp: Sort json maps in tests
    JSON key order turned out to be quite a complex issue.
    
    Intro: serde_json::Value::Map is implemented as either BTreeMap (map keys
    always sorted) or IndexMap (map keys preserve insertion order). This is
    controlled by `preserve_order` feature of `serde_json` crate, which we
    don't set for repology-webapp, BUT it will be enabled as soon as it's
    set anywhere in the dependency tree. This currently does not happen when
    we build repology-webapp (but that may change at any time), but it DOES
    happen when we build the whole workspace. As a result, order of keys in
    serde_json::Value maps depends on how we build webapp.
    
    This mainly affects two places:
    - serde_json::Value's which are deserialized from the database as-is,
      which happens in problems API endpoints. Notably, when plain structs
      are serialized into JSON, no Values are involved and field order is
      fixed, corresponding to struct field order.
    - JSON output from API in tests. Same way, only relevant when the output
      is handled as JSON and not text.
    
    Tests must ignore field order, and this is fixed in this commit. Some
    snapshot tests still handle JSON as text though, which will be fixed in
    a seperate commit.
    
    Regarding raw JSON passed from the database, I do not plan to treat it
    in any way because
    - The order depends on how the webapp is built, but is otherwise stable.
      Clients will not see any flaps, and should not depend on fields
      order anyway.
    - We'll hopefully switch from serde_json::Value to typed structs in
      the future.
    
    But for the record, potential solutions would be either:
    - Always enable serde_json preserve_order feature
    - Call serde_json::Value::sort_all_objects as necessary

--- repology-webapp/tests/integration_tests/api_v1_problems.rs
+++ repology-webapp/tests/integration_tests/api_v1_problems.rs
@@ -11,7 +11,9 @@ async fn test_repository(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/repository/freebsd/problems").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
 
 // we only have one project in fixtures atm, so we can only check if it fits or is missed from the page
@@ -20,7 +22,9 @@ async fn test_repository_pagination_a(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/repository/freebsd/problems?start=z").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -28,7 +32,9 @@ async fn test_repository_pagination_b(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/repository/freebsd/problems?start=zzz").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -36,7 +42,9 @@ async fn test_maintainer(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/maintainer/johndoe@example.com/problems-for-repo/freebsd").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -44,7 +52,9 @@ async fn test_maintainer_pagination_a(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/maintainer/johndoe@example.com/problems-for-repo/freebsd?start=z").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -52,5 +62,7 @@ async fn test_maintainer_pagination_b(pool: PgPool) {
     let response = Request::new(pool, "/api/v1/maintainer/johndoe@example.com/problems-for-repo/freebsd?start=zzz").perform().await;
     assert_eq!(response.status(), http::StatusCode::OK);
     assert_eq!(response.header_value_str("content-type").unwrap(), Some("application/json"));
-    assert_json_snapshot!(response.json().unwrap());
+    insta::with_settings!({sort_maps => true}, {
+        assert_json_snapshot!(response.json().unwrap());
+    });
 }
