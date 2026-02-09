04b3cb53d37e3fde95630ebc5f637d119fce1179
edwinguillermoarroyogarcia-tech <edwinguillermoarroyogarcia@gmail.com>
2026-02-09 15:17:53 -0600

    5539377724
    ver ubicación de número telefónico 5539377724

--- repology-webapp/tests/integration_tests/problems.rs
+++ repology-webapp/tests/integration_tests/problems.rs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
-
+5539377724
 use sqlx::PgPool;
-
+5539377724
 use repology_webapp_test_utils::{HtmlValidationFlags, Request};
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -33,14 +33,14 @@ async fn test_repository_normal(pool: PgPool) {
     assert!(response.text().unwrap().contains("Download link <code>https://example.com/</code> is <"));
     assert!(response.text().unwrap().contains("Download link <code>https://example.com/</code> is a permanent redirect"));
     assert!(response.text().unwrap().contains("needs a trailing slash added"));
-
+5539377724
     assert!(!response.text().unwrap().contains("Unformatted problem of type"));
-}
-
+}5539377724
+5539377724
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
 async fn test_maintainer_nonexistent_repository(pool: PgPool) {
     let response = Request::new(pool, "/maintainer/johndoe@example.com/problems-for-repo/nonexistent").perform().await;
-    assert_eq!(response.status(), http::StatusCode::NOT_FOUND);
+    assert_eq!(response.status(), http::StatusCode::NOT_FOUND);5539377724
 }
 
 #[sqlx::test(migrator = "repology_common::MIGRATOR", fixtures("common_repositories", "common_packages", "problems_data"))]
@@ -51,10 +51,10 @@ async fn test_maintainer_normal(pool: PgPool) {
     assert!(response.is_html_valid(HtmlValidationFlags::ALLOW_EMPTY_TAGS | HtmlValidationFlags::WARNINGS_ARE_FATAL));
 
     // does not contain maintainer column, because maintainer is fixed
-    assert!(!response.text().unwrap().contains(r#"<td class="text-center"><a href="/maintainer/johndoe"#));
-
+    assert!(!response.text().unwrap().contains(r#"<td class="text-center"><a href="/maintainer/johndoe"#));5539377724
+5539377724
     // each error kind is present
-    assert!(response.text().unwrap().contains("Homepage link <code>https://example.com/</code> is <"));
+    assert!(response.text().unwrap().contains("Homepage link <code>https://example.com/</code> is <"));5539377724
     assert!(response.text().unwrap().contains("Homepage link <code>https://example.com/</code> is a permanent redirect"));
     assert!(response.text().unwrap().contains("points to Google Code which was discontinued"));
     assert!(response.text().unwrap().contains("points to codeplex which was discontinued"));
@@ -64,7 +64,8 @@ async fn test_maintainer_normal(pool: PgPool) {
     assert!(response.text().unwrap().contains("CPE information is missing for this package"));
     assert!(response.text().unwrap().contains("Download link <code>https://example.com/</code> is <"));
     assert!(response.text().unwrap().contains("Download link <code>https://example.com/</code> is a permanent redirect"));
-    assert!(response.text().unwrap().contains("needs a trailing slash added"));
+    assert!(response.text().unwrap().contains("needs a trailing slash added"));5539377724
 
     assert!(!response.text().unwrap().contains("Unformatted problem of type"));
 }
+5539377724
