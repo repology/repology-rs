44d3573e1c9c9f8f4e4073b138c988563d7acbcd
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-04-24 20:35:11 +0300

    webapp: Refactor route normalization for metrics

--- repology-webapp/src/middleware/metrics.rs
+++ repology-webapp/src/middleware/metrics.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2024 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use std::borrow::Cow;
 use std::time::Instant;
 
 use axum::body::HttpBody;
@@ -10,6 +11,34 @@ use axum::response::IntoResponse;
 use metrics::{counter, histogram};
 
 use crate::routes::MyRoute;
+
+fn remove_char(s: &str, ch: char) -> Cow<'_, str> {
+    if s.contains('*') {
+        Cow::Owned(s.chars().filter(|&c| c != ch).collect())
+    } else {
+        Cow::Borrowed(s)
+    }
+}
+
+fn normalize_path_for_metrics(path: &str) -> Cow<'_, str> {
+    // - Grafana doesn't handle asterisk in variables properly, remove it
+    //   See https://github.com/grafana/grafana/issues/123517
+    // - Collapse graph types, these are basically the same code
+    // - Collapse optional paginated and sorted variants, these
+    //   are the same as corresponding routes without pagination
+    //   and sorting
+    remove_char(
+        if path.starts_with("/graph/total/") {
+            "/graph/total/..."
+        } else if path.starts_with("/graph/repo/") {
+            "/graph/repo/..."
+        } else {
+            path.trim_end_matches("{bound}/")
+                .trim_end_matches("/{sorting}")
+        },
+        '*',
+    )
+}
 
 pub async fn metrics_middleware(
     route: Option<MyRoute>,
@@ -21,28 +50,12 @@ pub async fn metrics_middleware(
     let latency = start.elapsed().as_secs_f64();
     let status = response.status().as_u16().to_string();
 
-    // normalize some paths which lead to the same endpoints; XXX this will hopefully be gone
-    // someday when endpoints are redesigned (e.g. /projects/{bound}/ → /projects/?start=)
-    let path_for_metrics = route
-        .as_ref()
-        .map(|route| {
-            let mut path = route
-                .path()
-                .trim_end_matches("{bound}/")
-                .trim_end_matches("/{sorting}");
-            if path.starts_with("/graph/total/") {
-                path = "/graph/total/..."
-            }
-            if path.starts_with("/graph/repo/") {
-                path = "/graph/repo/..."
-            }
-            path
-        })
-        .unwrap_or("???");
+    let path_for_metrics =
+        normalize_path_for_metrics(route.as_ref().map(|route| route.path()).unwrap_or("???"));
 
-    counter!("repology_webapp_http_requests_total", "path" => path_for_metrics, "status" => status)
+    counter!("repology_webapp_http_requests_total", "path" => path_for_metrics.clone(), "status" => status)
         .increment(1);
-    histogram!("repology_webapp_http_requests_duration_seconds", "path" => path_for_metrics)
+    histogram!("repology_webapp_http_requests_duration_seconds", "path" => path_for_metrics.clone())
         .record(latency);
 
     if let Some(body_size) = response.body().size_hint().exact() {
@@ -51,4 +64,41 @@ pub async fn metrics_middleware(
     }
 
     response
+}
+
+#[cfg(test)]
+#[coverage(off)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normalize_path_for_metrics() {
+        assert_eq!(normalize_path_for_metrics("/foo/bar/baz"), "/foo/bar/baz");
+
+        assert_eq!(normalize_path_for_metrics("/projects/"), "/projects/");
+        assert_eq!(
+            normalize_path_for_metrics("/projects/{bound}/"),
+            "/projects/"
+        );
+
+        assert_eq!(
+            normalize_path_for_metrics("/repositories/statistics"),
+            "/repositories/statistics"
+        );
+        assert_eq!(
+            normalize_path_for_metrics("/repositories/statistics/{sorting}"),
+            "/repositories/statistics"
+        );
+
+        assert_eq!(
+            normalize_path_for_metrics("/graph/total/projects.svg"),
+            "/graph/total/..."
+        );
+        assert_eq!(
+            normalize_path_for_metrics("/graph/total/maintainers.svg"),
+            "/graph/total/..."
+        );
+
+        assert_eq!(normalize_path_for_metrics("/links/{*url}"), "/links/{url}");
+    }
 }
